7bc6f5c7d5b9710ce3c4ad5c1cd990e23fcc3682
Aditya Khaparkar <adityakhaparkarpuneiat@gmail.com>
2022-08-26 11:18:15 +0530

    changes in name

--- src/HomeScreen/HomeScreen.js/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js/HomeScreen.js
@@ -24,6 +24,7 @@ export default function HomeScreen() {
         <Button title='EDIT' onPress={() => {
           dispatch(editmodalopen(true))
           dispatch(index(index))
+          //ADI
         }} />
         
         <Button title='DELETE'  />
